2f91356da941014f711728a1600db649655fc7db
gorkemetem <gorkemetem98@gmail.com>
2025-10-24 22:02:32 +0300

    Add project files.

---
+++ Core.Persistence/Dynamıc/Filter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Dynamıc;
+
+public class Filter
+{
+    public string Field { get; set; }
+    public string? Value { get; set; }
+    public string Operator { get; set; }
+    public string Logic { get; set; }
+
+    public IEnumerable<Filter> Filters { get; set; }
+
+    public Filter()
+    {
+        Field = string.Empty;
+        Operator = string.Empty;
+    }
+
+    public Filter(string Field, string @operator )
+    {
+        Field = Field;
+        Operator = @operator;
+    }
+
+}
---
+++ Core.Persistence/Paging/IQueryablePaginateExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Paging;
+
+public static class IQueryablePaginateExtensions
+{
+    public static async Task<Paginate<T>> ToPaginateAsync<T>(
+        this IQueryable<T> source,
+        int index,
+        int size,
+        CancellationToken cancellationToken = default)
+    {
+        int count = await source.CountAsync(cancellationToken).ConfigureAwait(false);
+        List<T> items = await  source.Skip(index * size).Take(size).ToListAsync(cancellationToken).ConfigureAwait(false);
+        
+        Paginate<T> List = new()
+        {
+            Index = index,
+            Size = size,
+            Count = count,
+            Pages = (int)Math.Ceiling((double)count / size),
+            Items = items
+        };
+
+        return List;
+    }
+
+    public static Paginate<T> ToPaginate<T>(
+        this IQueryable<T> source,
+        int index,
+        int size)
+    {
+        int count =  source.Count();
+        var items =  source.Skip(index * size).Take(size).ToList();
+
+        Paginate<T> List = new()
+        {
+            Index = index,
+            Size = size,
+            Count = count,
+            Pages = (int)Math.Ceiling((double)count / size),
+            Items = items
+        };
+
+        return List;
+    }
+}
